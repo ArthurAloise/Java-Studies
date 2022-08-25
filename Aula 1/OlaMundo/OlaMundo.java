6371df60aa400c9a71e90fa6e59bca798180203b
Arthur Aloise <103983950+ArthurAloise@users.noreply.github.com>
2022-08-25 01:43:58 -0400

    Update OlaMundo.java

--- Aula 1/OlaMundo/OlaMundo.java
+++ Aula 1/OlaMundo/OlaMundo.java
@@ -1,6 +1,6 @@
 public class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
-        System.out.print("Me livrei da maldição caralho!!");
+        System.out.print("Me livrei da maldição!!");
     }
 }
