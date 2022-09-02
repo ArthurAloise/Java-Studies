b5c3f32d8d9439aaa84f5bf707ac538bba5c8aca
Arthur Aloise <103983950+ArthurAloise@users.noreply.github.com>
2022-09-02 17:08:19 -0400

    Add files via upload

---
+++ Aula 7/Fundamentos/Imports.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+import java.util.Date;
+
+public class Imports {
+	public static void main(String[] args) throws Exception {
+		String s = "Bom dia";
+		System.out.println(s.toUpperCase());
+		
+		Date d = new Date(); // Exemplo Date.
+		System.out.println(d);
+	}
+
+}
