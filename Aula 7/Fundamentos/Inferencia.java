b2300ce4feb239245e8bdfdb7799c776160d6400
Arthur Aloise <103983950+ArthurAloise@users.noreply.github.com>
2022-09-02 17:09:17 -0400

    Add files via upload

---
+++ Aula 7/Fundamentos/Inferencia.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) throws Exception {
+		
+		double a = 12;
+		System.out.println(a);
+		a = 5.4;
+		System.out.println(a);
+		
+		var b = "Arthur";
+		System.out.println(b);
+		b = "Aloise";
+		System.out.println(b);
+		
+		double c; // Variável fi declarada.
+		c = 5.99; // Variável foi inicializada.
+		System.out.println(c); // Usada.
+		
+		//O.B.S.: Com "VAR" não da de fazer isso, ela tem que ser declarada em seguida.
+	}
+
+}
